230ac3d6262f9b4fa00fd32b61c84650f01c9989
Akash Verma <aakashverma7184@gmail.com>
2023-09-17 23:53:55 +0530

    Rotating the circle

--- main.cpp
+++ main.cpp
@@ -1,18 +1,26 @@
 #include <cmath>
 #include <iostream>
+#include <unistd.h>
 using namespace std;
+
+void clearScreen() { std::cout << "\033[2J\033[H"; }
 
 int main() {
   int radius = 20;
 
-  for (int i = -radius; i <= radius; i++) {
-    for (int j = -radius; j <= radius; j++) {
-      if (round(sqrt(i * i + j * j)) == radius) {
-        cout << "* ";
-      } else {
-        cout << "  ";
+  for (float a = 0; a <= 2 * M_PI; a += 0.1) {
+    clearScreen();
+    for (int i = -radius; i <= radius; i++) {
+      for (int j = -radius; j <= radius; j++) {
+        if (round(sqrt(pow(i * cos(a), 2) + j * j)) ==
+            abs(round(radius * cos(a)))) {
+          cout << "* ";
+        } else {
+          cout << "  ";
+        }
       }
+      cout << "\n";
     }
-    cout << "\n";
+    usleep(100000);
   }
 }
